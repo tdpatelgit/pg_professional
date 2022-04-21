51dc74d3a5abeb81be807ed1fa8d3eb4738992e8
Tejas D Patel <tdpatelmail@gmail.com>
2022-04-21 22:25:00 +0530

    feat: Integrated logging in python

--- src/server.py
+++ src/server.py
@@ -1,7 +1,12 @@
 """Main server file."""
 import os
+import logging
 from datetime import timedelta, datetime
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+
+from src.utils import setup_log
+
+setup_log(__file__)
 
 template_dir = os.path.abspath('./src/templates')
 app = Flask(__name__, template_folder=template_dir)
@@ -15,12 +20,14 @@ auth_data = {'Tejas': 'abcd'}
 @app.route('/home')
 def home():
     """Home route."""
+    logging.info('Route :: / or /home')
     return render_template('home.html')
 
 
 @app.route('/guest')
 def guest():
     """Gest route."""
+    logging.info('Route :: /guest')
     if 'user' in session:
         return render_template('welcome.html')
 
@@ -30,18 +37,21 @@ def guest():
 @app.route('/admin')
 def admin():
     """Admin route."""
+    logging.info('Route :: /admin')
     return render_template('admin.html')
 
 
 @app.route('/rent')
 def rent():
     """Rent route."""
+    logging.info('Route :: /rent')
     return render_template('rent.html')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     """Login route."""
+    logging.info(f'Route :: /login {request.method}')
     if request.method == 'POST':
         session.permanent = True
         user = request.form['nm']
@@ -63,6 +73,7 @@ def login():
 @app.route('/logout')
 def logout():
     """Logout route."""
+    logging.info('Route :: /logout')
     if 'user' in session:
         session.pop('user', None)
         flash('You have logged out successfully', 'info')
---
+++ src/utils.py
@@ -0,0 +1,10 @@
+"""Utility functions."""
+import os
+import logging
+
+def setup_log(filename: str):
+    """Setup logging format."""
+    logging.basicConfig(
+        # filename='pg_pro.log',
+        level=logging.DEBUG,
+        format=f'%(asctime)s | {os.path.basename(filename)} | %(levelname)s | %(message)s')
